9b04f815a7b785d489ea78df4f8d57183ca37116
Dalma Greksza <dalmajuliag@gmail.com>
2023-01-31 16:45:36 +0100

    frontend start

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import {React} from 'react';
+import Form from 'react-bootstrap/Form';
+
+function MainPage() {
+    return (
+      <>
+      <Form.Text>Hello mainpage</Form.Text>
+      
+      </>
+    );
+  }
+  
+  export default MainPage;
---
+++ src/components/SecondPage.js
@@ -0,0 +1,14 @@
+import {React} from 'react';
+import Form from 'react-bootstrap/Form';
+
+function SecondPage() {
+    return (
+      <>
+      <Form.Text>Hello secondpage</Form.Text>
+      
+      
+      </>
+    );
+  }
+  
+  export default SecondPage;
